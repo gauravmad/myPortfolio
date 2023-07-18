360aacb930609dd6ea1e05a8e33027c42de1cad2
Gaurav Madan <gauravmadan2004@gmail.com>
2023-07-19 01:43:49 +0530

    updated home

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,10 +9,10 @@ export default function Navbar() {
                 <img src={gauravlogo} alt="gauravlogo" className='w-28 md:w-32 lg:w-48'/>
             </div>
             <ul className='flex md:gap-5 lg:gap-10'>
-                <li className='navlist text-white md:text-xl lg:text-2xl font-medium hover:text-cyan-500  cursor-pointer'>Home</li>
-                <li className='navlist text-white md:text-xl lg:text-2xl font-medium hover:text-cyan-500  cursor-pointer'>About Me</li>
-                <li className='navlist text-white md:text-xl lg:text-2xl font-medium hover:text-cyan-500  cursor-pointer'>Experience</li>
-                <li className='navlist text-white md:text-xl lg:text-2xl font-medium hover:text-cyan-500  cursor-pointer'>Contact</li>
+                <li className='navlist text-white md:text-xl lg:text-2xl font-normal hover:text-cyan-500  cursor-pointer'>Home</li>
+                <li className='navlist text-white md:text-xl lg:text-2xl font-normal hover:text-cyan-500  cursor-pointer'>About Me</li>
+                <li className='navlist text-white md:text-xl lg:text-2xl font-normal hover:text-cyan-500  cursor-pointer'>Experience</li>
+                <li className='navlist text-white md:text-xl lg:text-2xl font-normal hover:text-cyan-500  cursor-pointer'>Contact</li>
             </ul>
         </div>
       
